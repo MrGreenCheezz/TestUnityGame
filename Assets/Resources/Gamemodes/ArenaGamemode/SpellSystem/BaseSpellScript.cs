542a01f9e83c2dbff7a71d189d07db46885b9b3e
MrGreenCheezz <anonsandro@gmail.com>
2024-06-05 03:26:52 +0500

    Started work with spell system

---
+++ Assets/Resources/Gamemodes/ArenaGamemode/SpellSystem/BaseSpellScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseSpellScript : NetworkBehaviour
+{
+    public bool isReloading = false;
+    private float reloadTime = 0.0f;
+    public float TimeToReload = 1.0f;
+    public Image SpellImage;
+    public int Damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+   public virtual void Update()
+    {
+        if(IsOwner || IsServer)
+        {
+            if (isReloading)
+            {
+                reloadTime += Time.deltaTime;
+                if (reloadTime >= TimeToReload)
+                {
+                    isReloading = false;
+                    reloadTime = 0.0f;
+                }
+            }
+        }    
+    }
+
+    public virtual void CastSpell()
+    {
+        if (!isReloading)
+        {
+            StartReloading();
+        }
+    }
+
+    public void StartReloading()
+    {
+        isReloading = true;
+    }
+}
--- Assets/Resources/Gamemodes/ArenaGamemode/SpellSystem/PlayerSpellComponent.cs
+++ Assets/Resources/Gamemodes/ArenaGamemode/SpellSystem/PlayerSpellComponent.cs
@@ -1,19 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.Netcode;
 using UnityEngine;
 
 public class PlayerSpellComponent : NetworkBehaviour
 {
+    public List<BaseSpellScript> Spells = new List<BaseSpellScript>();
+    public int CurrentSpellIndex = 0;
+    public GameObject testSpawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddSpell(testSpawn);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ChangeSpellIndex(int index)
+    {
+        CurrentSpellIndex = index;
+    }
+
+    public void CastSpell()
+    {
+        Spells[CurrentSpellIndex].CastSpell();
+    }
+
+    public void AddSpell(GameObject spell)
+    {
+        if (IsServer)
+        {
+            AddSpellServer(spell);
+        }
+    }
+    public void AddSpellServer(GameObject obj)
+    {
+        var tmp = Instantiate(obj, transform);
+        tmp.GetComponent<NetworkObject>().SpawnWithOwnership(OwnerClientId);
+        tmp.transform.SetParent(transform);
+        AddSpellClientRpc(tmp.GetComponent<NetworkObject>());
+    }
+    [ClientRpc]
+    public void AddSpellClientRpc(NetworkObjectReference obj)
+    {
+        obj.TryGet(out NetworkObject networkObject);
+        Spells.Add(networkObject.gameObject.GetComponent<BaseSpellScript>());
     }
 }
